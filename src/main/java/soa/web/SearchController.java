ba5aa8e296476714da1d00eea94b7b623c9b1b4e
Iván Santamaría <ivansantamaria.m@gmail.com>
2017-12-22 23:56:58 +0100

    test

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Map;
+import java.util.HashMap;
 
 
 @Controller
@@ -23,6 +25,21 @@ public class SearchController {
     @RequestMapping(value="/search")
     @ResponseBody
     public Object search(@RequestParam("q") String q) {
+
+        Map<String, Object> map = new HashMap<>();
+        String clave;
+
+        if (q.contains("max:")) {
+            String[] q2 = q.split("max:");
+            String maximo = q2[1];
+            clave = q2[0];
+            map.put("CamelTwitterCount", maximo);
+        } else {
+            clave = q;
+        }
+
+        map.put("CamelTwitterKeywords", clave);
+
         return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
     }
 }
